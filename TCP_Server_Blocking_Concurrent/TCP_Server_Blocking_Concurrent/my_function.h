61203c4048f78099308f29ae1f9d69fcea7203d5
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2014-03-07 16:38:06 +0800

    添加函数声明

--- TCP_Server_Blocking_Concurrent/TCP_Server_Blocking_Concurrent/my_function.h
+++ TCP_Server_Blocking_Concurrent/TCP_Server_Blocking_Concurrent/my_function.h
@@ -1,22 +1,24 @@
 ﻿#ifndef MY_FUNCTION_H
 #define MY_FUNCTION_H
 
+#include <stdio.h>
 #include <WinSock2.h>
-#include <iostream>
 
-#define  BUFFER_SIZE		SO_MAX_MSG_SIZE	//缓冲区大小
-#define  DEF_PORT			10000
+#define		BUFFER_SIZE		SO_MAX_MSG_SIZE
+#define		DEF_PORT			10000
 
-// 创建一个监听套接字并把本地套接字地址(本地IP地址和本地端口)绑定到监听套接字
+#pragma comment(lib, "ws2_32.lib")
+
+// 创建一个监听套接字并把本地套接字地址(本地IP地址和本地端口)绑定到监听套接字                                                                     */
 SOCKET BindListen(void);
 
 // 接受客户端的连接请求
 SOCKET AcceptConnection(SOCKET hListenSocket);
 
-//接受客户端的数据并回显
+// 接受客户端的数据并回显
 bool ProcessConnection(SOCKET hClientSocket);
 
-//关闭当前客户端连接
+// 关闭当前客户端连接
 bool ShutdownConnection(SOCKET hClientSocket);
 
 // 服务器主题函数
@@ -24,5 +26,21 @@ void DoWork(void);
 
 //服务一个客户端连接的线程函数,参数lpParam就是客户端的套接字句柄
 DWORD WINAPI ThreadProc(LPVOID lpParam);
+
+//完整发送数据
+//s: 已连接套接字
+//data: 数据起始地址
+//数据大小
+//若成功发送,返回true;否者返回false.
+bool CompleteSend(SOCKET s, const char *data, int len);
+
+//完整接收数据
+//s : 已连接套接字
+//buffer : 数据起始地址
+//len :"数据大小
+//若成功接收,返回true;否者返回false.
+bool CompleteRecv(SOCKET s, char *buffer, int len);
+
+
 
 #endif
